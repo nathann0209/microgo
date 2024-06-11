69c9f26398a0812d13902cf77754ee6eb46e073f
nathann0209 <141504383+nathann0209@users.noreply.github.com>
2024-06-11 16:20:14 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nathann0209/microgo/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	helloHandler := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", helloHandler)
+
+	http.ListenAndServe(":9090", sm)
+}
